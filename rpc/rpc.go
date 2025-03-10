9102c1b55978b7ec146fed8771101318cb8a7a4c
Pawel Lipinski <lipinskijob@gmail.com>
2025-03-10 19:15:55 +0100

    Add Decode code

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func EncodeMessage(message any) string {
@@ -12,4 +15,21 @@ func EncodeMessage(message any) string {
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMesage(message []byte) (int, error) {
+	header, content, found := bytes.Cut(message, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find separator")
+	}
+
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	_ = content
+
+	return contentLength, nil
 }
