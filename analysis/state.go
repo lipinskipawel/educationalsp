bd29e64e039c9aca1d40105d936e7bb88e4cd7c1
Pawel Lipinski <lipinskijob@gmail.com>
2025-03-13 13:09:21 +0100

    More interactive hover

--- analysis/state.go
+++ analysis/state.go
@@ -1,5 +1,11 @@
 package analysis
 
+import (
+	"fmt"
+
+	"github.com/lipinskipawel/educationalsp/lsp"
+)
+
 type State struct {
 	// Map of file names to contents
 	Documents map[string]string
@@ -15,4 +21,20 @@ func (s *State) OpenDocument(uri, text string) {
 
 func (s *State) UpdateDocument(uri, text string) {
 	s.Documents[uri] = text
+}
+
+func (s *State) Hover(id int, uri string, position lsp.Position) lsp.HoverResponse {
+	// In real life, this would look up the type in our type analysis code...
+
+	document := s.Documents[uri]
+
+	return lsp.HoverResponse{
+		Response: lsp.Response{
+			RPC: "2.0",
+			ID:  &id,
+		},
+		Result: lsp.HoverResult{
+			Contents: fmt.Sprintf("File: %s, Characterd: %d", uri, len(document)),
+		},
+	}
 }
